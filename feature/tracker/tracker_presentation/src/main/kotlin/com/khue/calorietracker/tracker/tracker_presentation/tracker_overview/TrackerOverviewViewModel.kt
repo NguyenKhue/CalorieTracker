c04e4bed75e8d42af3d6b5761477d257e9611402
khue.nguyen <khue.nguyen@samsung.com>
2023-08-28 18:13:37 +0700

    Complete TrackerOverviewViewModel

--- feature/tracker/tracker_presentation/src/main/kotlin/com/khue/calorietracker/tracker/tracker_presentation/tracker_overview/TrackerOverviewViewModel.kt
+++ feature/tracker/tracker_presentation/src/main/kotlin/com/khue/calorietracker/tracker/tracker_presentation/tracker_overview/TrackerOverviewViewModel.kt
@@ -1,40 +1,121 @@
 package com.khue.calorietracker.tracker.tracker_presentation.tracker_overview
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.khue.calorietracker.core.common.util.UiEvent
 import com.khue.calorietracker.core.preferences.domain.preferences.Preferences
+import com.khue.calorietracker.core.ui.navigation.Route
 import com.khue.calorietracker.tracker.tracker_domain.use_case.TrackerUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TrackerOverviewViewModel  @Inject constructor(
+class TrackerOverviewViewModel @Inject constructor(
     preferences: Preferences,
     private val trackerUseCases: TrackerUseCases
-): ViewModel() {
+) : ViewModel() {
+
+    var state by mutableStateOf(TrackerOverviewState())
+        private set
 
     private val _uiEvent = Channel<UiEvent>()
     val uiEvent = _uiEvent.receiveAsFlow()
+
+    private var getFoodsForDateJob: Job? = null
+
     init {
         preferences.saveShouldShowOnboarding(false)
     }
 
 
-
     fun onEvent(event: TrackerOverviewEvent) {
         when (event) {
-            is TrackerOverviewEvent.OnNextDayClick -> onNextDayClick()
-            is TrackerOverviewEvent.OnPreviousDayClick -> onPreviousDayClick()
-            is TrackerOverviewEvent.OnToggleMealClick -> onToggleMealClick(event.meal)
-            is TrackerOverviewEvent.OnDeleteTrackedFood -> onDeleteTrackedFood(event.trackedFood)
+            is TrackerOverviewEvent.OnNextDayClick -> {
+                state = state.copy(
+                    date = state.date.plusDays(1)
+                )
+                refreshFoods()
+            }
+            is TrackerOverviewEvent.OnPreviousDayClick -> {
+                state = state.copy(
+                    date = state.date.minusDays(1)
+                )
+                refreshFoods()
+            }
+            is TrackerOverviewEvent.OnToggleMealClick -> {
+                state  = state.copy(
+                    meals = state.meals.map {
+                        if(it.name == event.meal.name) {
+                            it.copy(isExpanded = !it.isExpanded)
+                        } else it
+                    }
+                )
+            }
+            is TrackerOverviewEvent.OnDeleteTrackedFood -> {
+                viewModelScope.launch {
+                    trackerUseCases.deleteTrackedFood(event.trackedFood)
+                    refreshFoods()
+                }
+            }
+
             is TrackerOverviewEvent.OnAddFoodClick -> {
                 viewModelScope.launch {
+                    _uiEvent.send(
+                        UiEvent.Navigate(
+                            route = Route.SEARCH
+                                    + "/${state.date.dayOfMonth}"
+                                    + "/${state.date.monthValue}"
+                                    + "/${state.date.year}"
+                        )
+                    )
                 }
             }
         }
+    }
+
+    private fun refreshFoods() {
+        getFoodsForDateJob?.cancel()
+        getFoodsForDateJob = trackerUseCases
+            .getFoodsForDate(state.date)
+            .onEach { foods ->
+                val nutrientsResult = trackerUseCases.calculateMealNutrients(foods)
+                state = state.copy(
+                    totalCabs = nutrientsResult.totalCarbs,
+                    totalFat = nutrientsResult.totalFat,
+                    totalProtein = nutrientsResult.totalProtein,
+                    totalCalories = nutrientsResult.totalCalories,
+                    carbsGoal = nutrientsResult.carbsGoal,
+                    proteinGoal = nutrientsResult.proteinGoal,
+                    fatGoal = nutrientsResult.fatGoal,
+                    caloriesGoal = nutrientsResult.caloriesGoal,
+                    trackedFoods = foods,
+                    meals = state.meals.map {
+                        val nutrientForMeal = nutrientsResult.mealNutrients[it.mealType]
+                            ?: return@map it.copy(
+                                carbs = 0,
+                                protein = 0,
+                                fat = 0,
+                                calories = 0
+                            )
+
+                        it.copy(
+                            carbs = nutrientForMeal.carbs,
+                            protein = nutrientForMeal.protein,
+                            fat = nutrientForMeal.protein,
+                            calories = nutrientForMeal.calories
+                        )
+                    }
+                )
+            }
+            .launchIn(viewModelScope)
     }
 }
