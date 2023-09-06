1bd4325aac90fa82ef9b24e3456372d1104f4b18
NguyenKhue <nguyenkhue09032001@gmail.com>
2023-09-06 22:39:02 +0700

    Update Hilt

--- build-logic/convention/src/main/java/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidHiltConventionPlugin.kt
@@ -24,16 +24,14 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("dagger.hilt.android.plugin")
-                // KAPT must go last to avoid build warnings.
-                // See: https://stackoverflow.com/questions/70550883/warning-the-following-options-were-not-recognized-by-any-processor-dagger-f
-                apply("org.jetbrains.kotlin.kapt")
+                apply("com.google.devtools.ksp")
             }
 
             dependencies {
                 add("implementation",libs.findLibrary("hilt.android").get())
-                add("kapt",libs.findLibrary("hilt.compiler").get())
+                add("ksp",libs.findLibrary("hilt.compiler").get())
                 add("androidTestImplementation", libs.findLibrary("hilt.android.testing").get())
-                add("kaptAndroidTest", libs.findLibrary("hilt.compiler").get())
+                add("kspAndroidTest", libs.findLibrary("hilt.compiler").get())
             }
 
         }
